58a70f31cc00277a3e55af3ae0a0fccfdac45e00
112AB0059 <146065279+112AB0059@users.noreply.github.com>
2023-11-07 11:45:12 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 double manualRound(double value, int decimalPlaces) {
     double multiplier = 1;
     for (int i = 0; i < decimalPlaces; i++) {
@@ -16,18 +17,19 @@ int main() {
     scanf("%d", &minutes);
 
     double cost;
-    
+
     if (minutes <= 800) {
         cost = minutes * 0.9;
     } else if (minutes > 800 && minutes <= 1500) {
-        cost = 800 * 0.9 + (minutes - 800) * 0.9 * 0.9;
+        cost = (800 * 0.9 + (minutes - 800) * 0.9)* 0.9;
     } else {
-        cost = 800 * 0.9 + 700 * 0.9 * 0.9 + (minutes - 1500) * 0.9 * 0.79;
+        cost = (800 * 0.9 + 700 * 0.9 + (minutes - 1500) * 0.9) * 0.79;
     }
 
-    cost = manualRound(cost, 1); // 使用手動四捨五入函數
-
-    printf("通話費用為：%.1lf\n", cost);
+    // 手動四捨五入到小數點以下第一位
+    double roundedCost =(cost * 10) / 10;
+    roundedCost = manualRound(roundedCost, 1);
+    printf("通話費用為：%.1lf\n", roundedCost);
 
     return 0;
 }
